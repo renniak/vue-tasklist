5f53395d948cb15f286b0cdad88e4d303fa0b779
James <36522308+renniak@users.noreply.github.com>
2021-02-18 16:33:00 -0300

    Add and clear login done

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const app = new Vue({
   el: "#app",
   data: {
-    task: "",
-    tasks: [],
+    newTask: {},
+    tasks: [
+      { name: "this is task one", isDone: false },
+      { name: "this is task two", isDone: false },
+      { name: "this is task three", isDone: false },
+    ],
   },
   methods: {
     addTask: function () {
-      if(this.task) this.tasks.push(this.task)
-      this.task = ""
+      this.tasks.push({ name: this.newTask.name, isDone: false });
+      this.newTask.name = "";
     },
     clearAllTasks: function () {
       this.tasks.splice(0,this.tasks.length)
-    }
+    },
   },
 });
