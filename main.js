cef152f7b5b8bf959a12eb8b25e8cebeaeceba24
James <36522308+renniak@users.noreply.github.com>
2021-02-18 17:49:59 -0300

    save tasks to locale storage

--- main.js
+++ main.js
@@ -2,19 +2,26 @@ const app = new Vue({
   el: "#app",
   data: {
     newTask: {},
-    tasks: [
-      { name: "this is task one", isDone: false },
-      { name: "this is task two", isDone: false },
-      { name: "this is task three", isDone: false },
-    ],
+    tasks: [],
   },
   methods: {
     addTask: function () {
-      this.tasks.push({ name: this.newTask.name, isDone: false });
-      this.newTask.name = "";
+      if (this.newTask.name) {
+        this.tasks.push({ name: this.newTask.name, isDone: false });
+        this.newTask.name = "";
+        this.updateTasks();
+      }
+    },
+    updateTasks: function () {
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      console.log("tasks updated");
     },
     clearAllTasks: function () {
-      this.tasks.splice(0,this.tasks.length)
+      this.tasks.splice(0, this.tasks.length);
     },
+  },
+  mounted() {
+    if (localStorage.tasks)
+      this.tasks = JSON.parse(localStorage.getItem("tasks"));
   },
 });
